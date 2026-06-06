2f076cc6cc80c1f9614563d9a0b7d36ee3981030
sadiepeytonn-netizen <sadiepeytonn@gmail.com>
2026-06-05 20:29:12 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
+import { AnimatePresence } from 'framer-motion'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import ServicesPage from './pages/ServicesPage'
 import AboutPage from './pages/AboutPage'
 import ContactPage from './pages/ContactPage'
 
-export default function App() {
+function AnimatedRoutes() {
+  const location = useLocation()
   return (
-    <Router>
-      <Navbar />
-      <Routes>
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
         <Route path="/" element={<Home />} />
         <Route path="/services" element={<ServicesPage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/contact" element={<ContactPage />} />
       </Routes>
+    </AnimatePresence>
+  )
+}
+
+export default function App() {
+  return (
+    <Router>
+      <Navbar />
+      <AnimatedRoutes />
     </Router>
   )
 }
